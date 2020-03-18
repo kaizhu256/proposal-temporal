02f87ad71c01f97512dd0f4fc0fe5657c0bb85fe
kai zhu <kaizhu256@gmail.com>
2020-03-17 19:55:30 -0500

    Merge doc/package.json into package.json. Move script /docs/build-html.js to /docs/build-docs.js.

--- build-docs.js
+++ build-docs.js
@@ -7,25 +7,35 @@
  */
 /*jslint node*/
 "use strict";
+let directoryOutDocs;
 let fs;
 let head;
 let marked;
 let path;
 let tail;
+// chdir to /docs/
+process.chdir("docs");
 // require modules
 fs = require("fs");
 marked = require("marked");
 path = require("path");
 // init toplevel variables
+directoryOutDocs = path.normalize("../out/docs/0").slice(0, -1);
 head = fs.readFileSync("head.html.part", "utf8");
 tail = fs.readFileSync("tail.html.part", "utf8");
-// convert files *.md to *.html
+// process files in /docs/
 fs.readdirSync(".").forEach(function (file) {
     let data;
-    // skip files with no .md extension
-    if (!(
-        /\.md$/
-    ).test(file)) {
+    switch (path.extname(file)) {
+    // copy files *.css to /out/docs
+    case ".css":
+        fs.copyFileSync(file, directoryOutDocs + file);
+        return;
+    // convert files *.md to *.html
+    case ".md":
+        break;
+    // skip remaining files
+    default:
         return;
     }
     data = fs.readFileSync(file, "utf8");
@@ -47,9 +57,12 @@ fs.readdirSync(".").forEach(function (file) {
     // prepend head.html.part and append tail.html.part
     data = head + data + tail;
     // save as .html
-    fs.writeFileSync(file.replace((
+    fs.writeFileSync(directoryOutDocs + file.replace((
         /\.md$/
     ), ".html"), data);
 });
 // copy README.html to index.html
-fs.writeFileSync("index.html", fs.readFileSync("README.html"));
+fs.copyFileSync(
+    directoryOutDocs + "README.html",
+    directoryOutDocs + "index.html"
+);
